bba05479bdd8b60f7143e9a3d06a59e52f8fa4d9
RafaelMalakian <raymondverletzer@gmail.com>
2026-03-10 21:09:16 -0400

    commit 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com pruebita
   apiUrl: 'https://maneja-tus-gastos-api-evqg.onrender.com',
 };
 
